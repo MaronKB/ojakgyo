ef4b71544c4b19d1dfd2f200c6dc751d9fedf47c
MRKB <64984647+MaronKB@users.noreply.github.com>
2024-11-25 14:25:00 +0900

    init

---
+++ src/main/java/com/devgang/ojakgyo/OjakgyoApplication.java
@@ -0,0 +1,13 @@
+package com.devgang.ojakgyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjakgyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjakgyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devgang/ojakgyo/OjakgyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgang.ojakgyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjakgyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
