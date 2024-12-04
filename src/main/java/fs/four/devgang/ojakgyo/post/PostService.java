8b432ea1ccdab6820eee6662212fa72080689bf2
minsuleefromnowhere <cw01094@gmail.com>
2024-12-05 00:21:00 +0900

    community main

--- src/main/java/fs/four/devgang/ojakgyo/post/PostService.java
+++ src/main/java/fs/four/devgang/ojakgyo/post/PostService.java
@@ -1,4 +1,15 @@
 package fs.four.devgang.ojakgyo.post;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PostService {
+
+    @Autowired
+    public PostDAO postDAO;
+
+    public void createPost(Post post) {
+        postDAO.insertPost(post);
+    }
 }
