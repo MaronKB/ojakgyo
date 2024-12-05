bd0085fc727d45df0c750ad05619c5895a7812f8
minsuleefromnowhere <cw01094@gmail.com>
2024-12-06 00:00:55 +0900

    community main

--- src/main/java/fs/four/devgang/ojakgyo/post/controller/PostController.java
+++ src/main/java/fs/four/devgang/ojakgyo/post/controller/PostController.java
@@ -1,4 +1,11 @@
 package fs.four.devgang.ojakgyo.post.controller;
 
-public class PostController {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+
+public interface PostController {
+
+    public ModelAndView listPost(HttpServletRequest request, HttpServletResponse response) throws Exception;
+    public ModelAndView postPage(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
