773bb007053e14049c36f52aba5dae90fced0fff
gnw <apmidbug@naver.com>
2024-12-09 21:23:25 +0900

    gnw

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -1,8 +1,11 @@
 //마이페이지
 // 버튼 클릭 이벤트 리스너 추가
+/*
 $(document).ready(function() {
     $("#editbutton").on("click", function() {
         alert("ajax 통신");
+*/
+
 /*        $.ajax()*/
 
        /* type: "GET",
@@ -18,5 +21,8 @@ $(document).ready(function() {
             alert("에러 발생: " + error); // 에러 메시지 출력
        }
     });*/
-    }
-});
+
+
+function btn(editbutton){
+    console.log('버튼이 클릭되었습니다');
+};
--- src/main/webapp/WEB-INF/views/user/mypage.jsp
+++ src/main/webapp/WEB-INF/views/user/mypage.jsp
@@ -1,4 +1,5 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jstl/core" %>
+<script src="<c:url value="/js/mypage.js"/>"></script>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
