8c9b32df196ca89f1b9808ff05e3becd9f6cb319
lee1345 <swa1129@naver.com>
2024-12-12 12:33:10 +0900

    12/12 오전작업

--- src/main/java/fs/four/devgang/ojakgyo/mypage/vo/MypageVO.java
+++ src/main/java/fs/four/devgang/ojakgyo/mypage/vo/MypageVO.java
@@ -1,24 +1,79 @@
 package fs.four.devgang.ojakgyo.mypage.vo;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.stereotype.Component;
-
-import java.sql.Date;
-
-@Setter
-@Getter
-@Component("MypageVO")
 public class MypageVO {
 
-    private String user_id;
-    private String user_email;
-    private String user_pw;
-    private String user_nickname;
-    private String user_intro;
+    private String userId; // USER_ID
+    private String userPw; // USER_PW
+    private String userNickname; // USER_NICKNAME
+    private String userEmail; // USER_EMAIL
+    private String userIntro; // USER_INTRO
+    private String userProfileName; // USER_PROFILE_NAME
+    private String userProfilePath; // USER_PROFILE_PATH
+    private String userAddress; // USER_ADDRESS
 
-    public MypageVO() {
+    // Getters and Setters
 
+    public String getUserId() {
+        return userId;
     }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPw() {
+        return userPw;
+    }
+
+    public void setUserPw(String userPw) {
+        this.userPw = userPw;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserIntro() {
+        return userIntro;
+    }
+
+    public void setUserIntro(String userIntro) {
+        this.userIntro = userIntro;
+    }
+
+    public String getUserProfileName() {
+        return userProfileName;
+    }
+
+    public void setUserProfileName(String userProfileName) {
+        this.userProfileName = userProfileName;
+    }
+
+    public String getUserProfilePath() {
+        return userProfilePath;
+    }
+
+    public void setUserProfilePath(String userProfilePath) {
+        this.userProfilePath = userProfilePath;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
 }
